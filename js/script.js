422b2a7755ebcfd64c08b28f7aa74c8f55baa125
rafi0012 <rafiultalukdar007@gmail.com>
2025-08-27 22:15:37 +0600

    Added Heart Count in card

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("Script JS");
+// customs function called
+function getElement(id) {
+    const element = document.getElementById(id).innerText;
+    return element;
+}
+
+// heart count 
+const hearts = document.querySelectorAll(".fa-heart");
+let heartCount = parseInt(getElement("heart-count")); 
+
+for (let i = 0; i < hearts.length; i++) {
+  hearts[i].addEventListener("click", function() {
+    heartCount++; 
+    document.getElementById("heart-count").innerText = heartCount;
+  });
+}
+
+
+
+
+
+
+
