e4cf0974ff6d7b1d046fb828acdcb4235bc0b0bb
rafi0012 <rafiultalukdar007@gmail.com>
2025-08-28 11:51:33 +0600

    Added Copy Function

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function getElement(id) {
     return element;
 }
 
-// heart count 
+// Heart Count 
 const hearts = document.querySelectorAll(".fa-heart");
 let heartCount = parseInt(getElement("heart-count")); 
 
@@ -16,8 +16,23 @@ for (let i = 0; i < hearts.length; i++) {
 }
 
 
+// Copy Button 
+document.getElementById("card-items").addEventListener("click", function (e) {
+	if (e.target.classList.contains("copy-btn")) {
+		copyNumber(e.target);
+	}
+});
 
-
-
-
-
+function copyNumber(button) {
+	const counter = document.getElementById("copy-button");
+	counter.innerText = Number(counter.innerText) + 1;
+	const card = button.parentElement.parentElement.parentElement;
+	const number = card.getElementsByTagName("h2")[0].innerText;
+	navigator.clipboard.writeText(number).then(function () {
+		alert("The number " + number + " has been copied successfully✅");
+		button.innerHTML = '<i class="fa-regular fa-circle-check pr-[8px]"></i>Copied';
+		setTimeout(function () {
+			button.innerHTML = '<i class="fa-regular fa-copy pr-[8px]"></i>Copy';
+		}, 1100);
+	});
+}
